bc034511d32b9a17a45475db135125c5ca2796e2
angeloDiepe <angelodiepe10@yahoo.com>
2020-06-04 12:20:02 +0100

    update playlist structure 1

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -22,9 +22,9 @@ class Playlist extends React.Component {
         />
 				</div>
         <div id="list-wrapper">
-        	{[0,1,2].map((tube,i)=>
+        	{this.props.tubes.map((tube,i)=>
         		<div className="vid" key={i+"vid"}>
-        			<span className="vid-play"><img src={play} alt="play" className="img"/>{this.props.currentTube.title}</span>
+        			<span className="vid-play"><img src={play} alt="play" className="img"/>{tube.title}</span>
         			<span className="duration">1:50</span>
         		</div>
         	)}
